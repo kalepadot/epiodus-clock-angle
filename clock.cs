33469425317632a877b578de30ea39eb8bedd105
jeremy kale padot and Zakkrey Short <jeremy@>
2020-02-24 14:39:30 -0800

    create clock angle calculator

--- clock.cs
+++ clock.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+
+public class Clock
 {
-  static void Main()
+  public static int Time(int hour, int minute)
   {
-    // program code goes here
+  int hourHand = ((hour * 30) + (minute/2));
+  int minuteHand = (minute*6);
+  int diffDegrees = Math.Abs(hourHand-minuteHand); 
+  int greater = 360 - diffDegrees;
+
+  if (diffDegrees > 180)
+  {
+    return (greater);
+  }
+  else
+  {
+    return (diffDegrees);
+  }
+  }
+  public static void Main()
+  {
+   Console.WriteLine("Please enter an hour 1-12");
+   string userHour = Console.ReadLine();
+   int hour = int.Parse(userHour);
+   Console.WriteLine("please enter a minute 00-59");
+   string userMinute = Console.ReadLine();
+   int minute = int.Parse(userMinute);
+
+   int result = Time(hour, minute);
+  Console.WriteLine(result);
   }
 }
+
